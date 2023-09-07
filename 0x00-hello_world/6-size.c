d189cb33b22f6283c4b3b1f8163b3d8eefd7948d
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-07 08:59:21 -0700

    Solve 5 and 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int int1;
+long int longint;
+long long int long2int;
+char char1;
+float float1;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char1));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int1));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longint));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long2int));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float1));
+
+return (0);
+}
