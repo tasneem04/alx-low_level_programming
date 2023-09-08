42a3e282effd8cff9e6a8c4f326f671ac2cf908c
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-08 15:36:21 -0700

    Fix 6 and 7

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i < 10; i++)
-		putchar(i);
-	putchar("\n");
+for ( i = 0; i < 10; i++ )
+putchar(i);
+putchar("\n");
 
-	return (0);
+return (0);
 }
