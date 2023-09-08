ff05b0ada5528e5c13cb92321b41649f1e19a6e0
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-08 16:13:19 -0700

    Fix 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,15 @@
  * Return: Always 0 (Success)
  */
 
-
-
-int main (void) {
+int main(void)
+{
 char ch;
 
-for( ch = 'a' ; ch <= 'z' ; ch++ ) 
+for (ch = 'a' ; ch <= 'z' ; ch++) 
 putchar(ch);
 
-for( ch = 'A' ; ch <= 'Z' ; ch++ )
+for (ch = 'A' ; ch <= 'Z' ; ch++)
 putchar(ch);
 putchar('\n');
-   
-   
 return(0);
 }
