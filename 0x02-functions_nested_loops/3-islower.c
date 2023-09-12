5792fe63467bce5ddfcfde9d3f1fb7391c25a3cc
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-12 09:45:46 -0700

    Solve 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * islower - check if c is lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+    if (islower(c) != 0 )
+    return (1);
+    else
+    return (0);
+}
