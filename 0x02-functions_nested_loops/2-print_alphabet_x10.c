7586c98a1a069595d113900dafede8fcb1a37f18
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-12 09:28:33 -0700

    Solve 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_alphabetx10 - print from a to z in lower cases
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+int i;
+
+for (i=0 ; i <=9 ; i++)
+{
+for (c='a' ; c<='z'; c++)
+printf("%c", c);
+
+printf("\n");
+}
+
+}
