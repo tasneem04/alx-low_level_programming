890f9dfa25a5721f44a52b9806421c0eb1561d1e
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-12 11:13:59 -0700

    Solve 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_last_digit - print the last digit 
+ *
+ * 
+ */
+int print_last_digit(int c)
+{
+int res;
+res = c%10;
+return (res);
+}
+    
