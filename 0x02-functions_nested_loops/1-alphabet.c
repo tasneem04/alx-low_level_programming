5e1e4893c81fb9f98078493fe5ffaac3618862f4
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-12 13:08:26 -0700

    Fix 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,11 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void){
+void print_alphabet(void)
+{
 char c;
-for (c='a' ; c<='z'; c++)
+for (c = 'a' ; c <= 'z'; c++)
 _putchar(c);
 
 _putchar('\n');
-
 }
