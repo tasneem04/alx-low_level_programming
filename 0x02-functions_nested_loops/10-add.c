8dc505848d7f8a22944f0e585b1ac9120a4d0c98
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-12 12:55:24 -0700

    Solve 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * add - add 2 int number 
+ */
+
+int add(int a , int b)
+{
+int res ;
+res = a+b;
+return (res);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _abs - return the obolute value  
+ *
+ * 
+ */
+
+int _abs(int c)
+{
+int res ;
+res = abs(c);
+return (res);
+}
