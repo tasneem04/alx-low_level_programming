032f1ebdfc8b1f37f7e28bb40227804c0d065217
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-12 11:31:08 -0700

    Solve 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,14 @@
  */
 int print_last_digit(int c)
 {
-int res;
-res = c%10;
-return (res);
+int r;
+
+if (r < 0)
+n = -1 * (r % 10);
+else
+n = r % 10;
+_putchar((n % 10) + '0');
+
+return (n % 10);
 }
     
