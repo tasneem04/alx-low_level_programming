98195bc8fabd46d0c6ea28c87244a359c19e3357
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-12 09:05:09 -0700

    Solve 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+
+print_alhabet(void);
+
+return (0);
+}
+
+void print_alphabet(void)
+char c;
+for (c='a' ;c<='z';c++){
+_putchar(c);
+_putchar('\n');
+}
+
+}
