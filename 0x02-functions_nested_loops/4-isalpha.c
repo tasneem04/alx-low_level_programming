b2c22642e4d8ed27ae7e924cc9c81deaed437ff3
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-12 10:11:04 -0700

    Solve 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * isalpha - check if c is alphabet 
+ *
+ * 
+ */
+
+int _isalpha(int c)
+{
+if (isalpha(c) != 0 )
+return (1);
+else
+return (0);
+}
