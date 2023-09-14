0cfbe3e289398b963f36a81e9257aa64f6da865a
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-14 13:47:31 -0700

    Add 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,15 @@
    * followed by a new line.
    * Return: Always 0
  */
+
 void more_numbers(void)
 {
-	int i, j = 0;
+int i ,j;
+for (i = 0;i <= 9 ; i++)
+{
+for (j = 0; j <= 14;j++)
+_putchar(j);
 
-	for (; j < 10; j++)
-	{
-		for (i = 0; i <= 14; i++)
-		{
-			if (i > 9)
-				_putchar(i / 10 + '0');
-
-			_putchar(i % 10 + '0');
-		}
-		_putchar('\n');
-	}
+_putchar('\n');
 }
+
