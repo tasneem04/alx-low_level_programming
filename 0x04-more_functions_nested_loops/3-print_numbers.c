1cf4ce69bbee86a0a81b3f4587e22475eb1f9c22
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-14 06:25:07 -0700

    Solve 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - A function that shows digits.
+ * @i: An input integer.
+ * Return: Nothing.
+ */
+void print_numbers(void)
+{
+int i;
+for (i=0; i<=9 ;i++){
+_putchar(i);
+_putchar("\n");
+}
