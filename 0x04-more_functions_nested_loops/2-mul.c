5ec9df4ae37cdc8310d79e882da8b0a891b19ca9
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-14 06:14:17 -0700

    Add 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - A function that mul digits.
+ * @a: An input integer.
+ * @b: An input integer.
+ * Return: integer digit.
+ */
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return (res);
+}
