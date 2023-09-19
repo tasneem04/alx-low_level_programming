deaa27197018368b34306e83811e034259eb7d83
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-19 01:38:34 -0700

    Add 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * swap_int - function that swaps two int
+ * @a int
+ * @b int
+ * Return - void
+ */
+
+void swap_int(int *a, int *b)
+{
+int swap1, swap2;
+swap1 = *a;
+swap2 = *b;
+*b = swap1 ;
+*a = swap2;
+}
