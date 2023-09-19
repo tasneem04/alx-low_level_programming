422e7b2eb6ecdeced1738fc5d23efd14a85a390a
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-19 12:42:01 -0700

    Fix 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/*
- * _puts - Write a function that prints a string, followed 
- *@str : pointer
- *Return void
+/**
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @str: an input string
+ * Return: Nothing
  */
 
 void _puts(char *str)
