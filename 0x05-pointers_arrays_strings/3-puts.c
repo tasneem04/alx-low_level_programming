a9fcd9673070defb07644ab49d1b9da17655ead1
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-19 02:53:28 -0700

    Solve 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,10 +8,10 @@ void _puts(char *str)
 {
 int i = 0;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
