1e51a1b7a8b969f54f41e67bd218b7d4d4e4240b
tasneem04 <eng.wr.tasneem@gmail.com>
2023-09-19 03:16:33 -0700

    Folve 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: an input string
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	for (s[i] != '\0')
+		i++;
+
+	while (i)
+		_putchar(s[--i]);
+
+	_putchar('\n');
+}
