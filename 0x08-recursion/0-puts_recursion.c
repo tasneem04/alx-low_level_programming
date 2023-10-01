35f23a73dcdee013bad5ccb45b02f2cd3d5861f2
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-01 07:29:33 -0700

    Fix 0 and 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,14 @@
  * @s: An input string to printing
  * Return: Nothing
  */
-void _puts_recursion(char *s) {
-    if (*s == '\0') {
-        putchar('\n');
-        return;
-    }
-
-    putchar(*s);
-    _puts_recursion(s + 1);
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+putchar('\n');
+return;
+}
+putchar(*s);
+_puts_recursion(s + 1);
 }
 
