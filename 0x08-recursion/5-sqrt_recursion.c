6ae2efb5a6172e96ef8559f7dbe8644aa9adaed2
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-01 08:21:52 -0700

    Fix 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * _sqrt_recursion - It returns the value of square root of n.
+ * @n: an input integer
+ * Return: The square root of n
+ */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return -1;
-    }
-    else if (n == 0 || n == 1)
-    {
-        return n;
-    }
-    else
-    {
-        return _sqrt_helper(n, 1);
-    }
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt(1, n));
 }
 
-int _sqrt_helper(int n, int guess)
+/**
+ * _sqrt - find square root
+ * @prev: previous value
+ * @root: square root value
+ * Return: the square root
+ */
+int _sqrt(int prev, int root)
 {
-    if (guess * guess == n)
-    {
-        return guess;  // Found the square root
-    }
-    else if (guess * guess > n)
-    {
-        return -1;     // Square root is not a whole number (not found)
-    }
-    else
-    {
-        return _sqrt_helper(n, guess + 1);  // Try the next guess
-    }
+	if (prev > root)
+		return (-1);
+	else if (prev * prev == root)
+		return (prev);
+	return (_sqrt(prev + 1, root));
 }
