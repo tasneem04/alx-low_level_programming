94e9769243e204097f199fc3f489e5c4095de813
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-01 08:50:39 -0700

    Solve 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _print_rev_recursion - a function that prints a string in reverse,
- * followed by a new line.
+ * _puts_recursion - a function that prints a string, followed by a new line.
  * @s: An input string to printing
  * Return: Nothing
  */
