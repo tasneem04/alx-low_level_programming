91138a4d50d839db41818813b18caf82ba4ff6a0
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-02 11:43:53 -0700

    Fix 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,11 @@
  * @argv: argument value, a strings that come after calling function
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[]) {
-    if (argc >= 1) {
-        printf("%s\n", argv[0]);
+int main(int argc, char *argv[])
+{
+    if (argc >= 1)
+    {
+printf("%s\n", argv[0]);
     }
     return 0;
 }
