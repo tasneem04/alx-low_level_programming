cbd591e8b45a5dcfc294e2db5f15f3ea823c7323
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-24 15:16:12 -0700

    Fix main.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 
@@ -15,3 +22,5 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
+#endif
