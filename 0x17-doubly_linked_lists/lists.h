4640a53a7bf0227cf5642c381a6930510a61529c
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-07 08:26:09 -0800

    Add 0

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,15 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+#include <stdio.h>
+#include <stdib.h>
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
